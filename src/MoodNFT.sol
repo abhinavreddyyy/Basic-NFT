aa07ebe2ff4d2a303a00331bdbb6cfe054c71730
abhinavreddyyy <abhinavreddyyy@gmail.com>
2026-01-27 20:53:26 +0530

    Added the metadata in the code

--- src/MoodNFT.sol
+++ src/MoodNFT.sol
@@ -1,8 +1,67 @@
-// SPDX-Licese-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNFT is ERC721 {
-    constructor() ERC721("MoodNFT", "MN") {}
+    uint256 private s_tokenCounter;
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageUri;
+
+    enum Mood {
+        SAD,
+        HAPPY
+    }
+
+    mapping(uint256 => Mood) private s_tokenIdToMood;
+
+    constructor(
+        string memory sadSvgImageUri,
+        string memory happySvgImageUri
+    ) ERC721("MoodNFT", "MN") {
+        s_tokenCounter = 0;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
+    }
+
+    function mintNFT() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY; // Default mood is HAPPY
+        s_tokenCounter++;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        string memory imageURI;
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            imageURI = s_happySvgImageUri;
+        } else {
+            imageURI = s_sadSvgImageUri;
+        }
+
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name": "',
+                                name(),
+                                '", "description": "An NFT that reflects the owners mood.", "attributes": [{"trait_type": "moodiness", "value": 100}], "image": "',
+                                imageURI,
+                                '"}'
+                            ) // created the JSON metadata and concatenated the strings together
+                        ) // gotta convert to bytes object in order to use the Base64 library
+                    ) // return the JSON metadata as a base64-encoded string
+                    // combined with the baseURI( data:application/json;base64,
+                ) // abi.encodePacked is used to concatenate strings
+            ); // return the final tokenURI string
+    }
 }
